74ae9f59ed432220189b9f8c712f2886a5180962
Abdul <abdulmusty03@outlook.com>
2025-03-07 17:14:00 +0100

    style: revamp customers say card

--- src/pages/IndustriesWeService/IWSCustomersSay.tsx
+++ src/pages/IndustriesWeService/IWSCustomersSay.tsx
@@ -1,4 +1,5 @@
 import CardAvatar from "@/assets/card-avatar.png";
+import CustomersSay from "@/assets/customers-say-card-bg.png";
 import Quotes from "@/assets/quotes.png";
 
 import { Card, CardContent } from "@/components/ui/card";
@@ -26,25 +27,32 @@ const IWSCustomersSay = () => {
                 <CarouselContent>
                     {Array.from({ length: 5 }).map((_, index) => (
                         <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 md:pl-6">
-                            <div className="p-1">
-                                <Card className="bg-[url(/src/assets/customers-say-card-bg.png)] bg-cover bg-center rounded-2xl border-none shadow-none">
-                                    <CardContent className="flex flex-col aspect-square justify-evenly">
-                                        <img src={Quotes} alt="Quotes" className="object-fill size-19.5" />
-                                        <p className="text-white text-lg">
-                                            “At TD, we've seen the transformative potential of AI to deliver more
-                                            personalized and intuitive experiences for our customers, colleagues and
-                                            communities.”
-                                        </p>
-                                        <div className="flex items-center gap-x-4">
-                                            <img src={CardAvatar} alt="Card avatar" className="object-fill size-12.5" />
-                                            <div className="space-y-1 text-white">
-                                                <h6>Olayinka Lexxa</h6>
-                                                <p className="font-light">VP, AI Technology Lead at TD Bank</p>
+                            <Card className="border-none shadow-none">
+                                <CardContent className="px-0">
+                                    <div className="flex relative isolate">
+                                        <img src={CustomersSay} alt="" className="absolute inset-0 object-fill size-full" />
+                                        <div className="flex flex-col aspect-square justify-evenly px-6 z-10">
+                                            <img src={Quotes} alt="Quotes" className="object-fill size-19.5" />
+                                            <p className="text-white text-lg">
+                                                “At TD, we've seen the transformative potential of AI to deliver more
+                                                personalized and intuitive experiences for our customers, colleagues and
+                                                communities.”
+                                            </p>
+                                            <div className="flex items-center gap-x-4">
+                                                <img
+                                                    src={CardAvatar}
+                                                    alt="Card avatar"
+                                                    className="object-fill size-12.5"
+                                                />
+                                                <div className="space-y-1 text-white">
+                                                    <h6>Olayinka Lexxa</h6>
+                                                    <p className="font-light">VP, AI Technology Lead at TD Bank</p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </CardContent>
-                                </Card>
-                            </div>
+                                    </div>
+                                </CardContent>
+                            </Card>
                         </CarouselItem>
                     ))}
                 </CarouselContent>
