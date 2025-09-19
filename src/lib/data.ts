0950e653fd2140bc6ef8bc0ecefd8019c97663c8
Abdul <abdulmusty03@outlook.com>
2025-09-19 13:06:48 +0100

    chore: revamp ai works page

---
+++ src/lib/data.ts
@@ -0,0 +1,19 @@
+import AuroraAiImg from "@/assets/aurora-ai.png";
+import claimixImg from "@/assets/claimix.png";
+import EngageXImg from "@/assets/engage-x.png";
+import PromptPlumberImg from "@/assets/prompt-plumber.png";
+import QuebitChatbotImg from "@/assets/quebit-chatbot.png";
+import RafaWater from "@/assets/rafa-water.png";
+import ScaleonImg from "@/assets/scaleon.png";
+import StudentAssistantChatbotImg from "@/assets/student-assistant-chatbot.png";
+
+export const ourProjects = [
+    { image: claimixImg, name: "CLAIMIX", tags: ["AI Automation", "AI Software"] },
+    { image: EngageXImg, name: "ENGAGE X", tags: ["AI Software"] },
+    { image: ScaleonImg, name: "SCALEON", tags: ["AI Automation", "AI Software"] },
+    { image: PromptPlumberImg, name: "PROMPT PLUMBER", tags: ["AI Software"] },
+    { image: AuroraAiImg, name: "AURORA AI", tags: ["AI Software"] },
+    { image: StudentAssistantChatbotImg, name: "STUDENT ASSISTANT CHATBOT", tags: ["AI Chatbot"] },
+    { image: QuebitChatbotImg, name: "QUEBIT CHATBOT", tags: ["AI Chatbot"] },
+    { image: RafaWater, name: "RAFA WATER", tags: ["AI Automation", "AI Chatbot"] },
+];
